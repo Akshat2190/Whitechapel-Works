d607f16221319f0841365e37d8aa5b2249c9c8de
Akshat Sharma <akshat12190@gmail.com>
2025-09-02 22:45:08 +0530

    commit

---
+++ server/controllers/messageController.js
@@ -0,0 +1,113 @@
+import axios from "axios";
+import Chat from "../models/chat.js";
+import User from "../models/user.js";
+import imagekit from "../configs/imageKit.js";
+import openai from "../configs/openai.js";
+
+
+// Text-based AI Chat Message Controller
+export const textMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        //check credits
+        if(req.user.credits < 2) {
+            return res.json({success: false, message: "Not enough credits"});
+        }
+
+        const {chatId, prompt} = req.body;
+
+        const chat = await Chat.findOne({userId, _id: chatId});
+
+        //✅ fallback check
+        if (!chat) {
+            return res.status(404).json({ success: false, message: "Chat not found" });
+        }
+
+        chat.messages.push({role: "user", content: prompt, timestamp: Date.now(), isImage: false});
+
+        const { choices } = await openai.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+
+        const reply = {
+            role: choices[0].message.role,
+            content: choices[0].message.content,
+            timestamp: Date.now(),
+            isImage: false
+          };
+         
+        res.json({success: true, reply});
+
+        chat.messages.push(reply);
+        await chat.save();
+        await User.updateOne({_id: userId}, {$inc: {credits: -1}})
+
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
+
+// Image Generation Message Controller
+export const imageMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        //check credits
+        if(req.user.credits < 2) {
+            return res.json({success: false, message: "Not enough credits"});
+        }
+        const {prompt, chatId, isPublished} = req.body;
+        //Find chat
+        const chat = await Chat.findOne({userId, _id: chatId});
+        //✅ fallback check
+        if (!chat) {
+            return res.status(404).json({ success: false, message: "Chat not found" });
+        }
+
+        //push user message to chat
+        chat.messages.push({role: "user", content: prompt, timestamp: Date.now(), isImage: false});
+
+        // Encode the prompt
+        const encodedPrompt = encodeURIComponent(prompt);
+
+        // Construct ImageKit AI generation URL
+        const genreratedImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodedPrompt}/
+        whitechapel_works/${Date.now()}.png?tr=w-800,h-800`; //transformation
+
+        //Trigger generation by fetching from Imagekit
+        const aiImageResponse = await axios.get(genreratedImageUrl, {responseType: 'arraybuffer'});
+
+        // Convert to Base64
+        const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString("base64")}`;
+
+        //upload to imagekit media library
+        const uploadResponse = await imagekit.upload({
+            file: base64Image,
+            fileName: `${Date.now()}.png`,
+            folder: "/whitechapel_works",
+        })
+
+        const reply = {
+            role: 'assistant',
+            content: uploadResponse.url,
+            timestamp: Date.now(),
+            isImage: true,
+            isPublished
+        }
+        res.json({success: true, reply});
+
+        chat.messages.push(reply);
+        await chat.save()
+
+        await User.updateOne({_id: userId}, {$inc: {credits: -2}})
+
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
+import chatRouter from './routes/chatRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+import creditRouter from './routes/creditRoutes.js';
+import { stripeWebhooks } from './controllers/webhooks.js';
+
+const app = express();
+
+await connectDB();
+
+// Stripe Webhooks
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res)=> res.send('Server is Live!'));
+app.use('/api/user', userRouter);
+app.use('/api/chat', chatRouter); // Note: This should be chatRouter in a real scenario
+app.use('/api/message', messageRouter);
+app.use('/api/credit', creditRouter);
+
+// port number to start backend server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
