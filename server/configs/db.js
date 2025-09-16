a83ff3f673986297c015cf08de8b921394cb1c80
Akshat Sharma <akshat12190@gmail.com>
2025-09-16 19:11:33 +0530

    commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log('Database is Connected'))
-        await mongoose.connect(`${process.env.MONGODB_URI}/Whitechapel_works`)
-    }catch (error) {
-        console.log(error.message)
-    }
-}
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database is Connected")
+    );
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export default connectDB;
